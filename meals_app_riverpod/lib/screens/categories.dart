d2131487908deff2eb5ec60d910521d06aa20153
Aung-Thu-Hein <aungthuhein225k@gmail.com>
2025-11-21 10:28:52 +0700

    feat: create project

---
+++ meals_app_riverpod/lib/screens/categories.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app_riverpod/data/dummy_data.dart';
+import 'package:meals_app_riverpod/models/category.dart';
+import 'package:meals_app_riverpod/models/meal.dart';
+import 'package:meals_app_riverpod/screens/meals.dart';
+import 'package:meals_app_riverpod/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  // final Category category;
+
+  const CategoriesScreen({super.key, required this.onToggleFavorite, required this.availableMeals});
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: 'Some Title',
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+    );
+  }
+}
